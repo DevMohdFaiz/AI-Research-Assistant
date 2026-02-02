57752a58a2ade02aaa332c0252ed8b08b2462606
DevMohdFaiz <shakirfaiz2602@gmail.com>
2026-02-02 23:06:34 +0100

    fixed api_key issue

--- config.py
+++ config.py
@@ -7,9 +7,22 @@ def _get_groq_api_key():
     if GROQ_API_KEY is None:
         try:
             GROQ_API_KEY = os.environ["GROQ_API_KEY"]
-        except KeyError:
-            raise "GROQ_API_KEY not set in the environment!"
+        except KeyError:  
+            GROQ_API_KEY = get_key(".env", "GROQ_API_KEY")  
+        except Exception as e:
+            raise KeyError("GROQ_API_KEY not set in the environment!")
     return GROQ_API_KEY
+    
+def _get_groq_api_key_2():
+    GROQ_API_KEY_2 = os.getenv("GROQ_API_KEY_2")
+    if GROQ_API_KEY_2 is None:
+        try:
+            GROQ_API_KEY_2 = os.environ["GROQ_API_KEY_2"]
+        except KeyError:
+            GROQ_API_KEY_2 = get_key(".env", "GROQ_API_KEY_2") 
+        except Exception as e:
+            raise KeyError("GROQ_API_KEY_2 not set in the environment!")
+    return GROQ_API_KEY_2
 
 def _get_tavily_api_key():
     TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
@@ -17,8 +30,11 @@ def _get_tavily_api_key():
         try:
             TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
         except KeyError:
-            raise "TAVILY_API_KEY not set in the environment!"
+            TAVILY_API_KEY = get_key(".env", "TAVILY_API_KEY") 
+        except Exception as e:
+            raise KeyError("TAVILY_API_KEY not set in the environment!")
     return TAVILY_API_KEY   
 
 GROQ_API_KEY =_get_groq_api_key()
+GROQ_API_KEY_2 =_get_groq_api_key_2()
 TAVILY_API_KEY =_get_tavily_api_key()
