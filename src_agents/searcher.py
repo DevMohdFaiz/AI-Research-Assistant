a872fde6d7ebea25c43758be1307f11b7f5e57e2
DevMohdFaiz <shakirfaiz2602@gmail.com>
2026-02-02 23:09:58 +0100

    fixed more bugs

--- src_agents/searcher.py
+++ src_agents/searcher.py
@@ -1,8 +1,7 @@
 import os
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
-from langchain_tavily import TavilySearch, tavily_search
-from langchain_community.tools import tool
+from langchain_tavily import TavilySearch
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
@@ -12,18 +11,25 @@ from config import TAVILY_API_KEY, GROQ_API_KEY, GROQ_API_KEY_2
 
 
 class SearchAgent:
-    "Make web searches based on the user topic"
+    "Make the web for sources to conduct the research"
 
     def __init__(self):
         self.tailvy_search = TavilySearch(
-            tavily_api_key=TAVILY_API_KEY, search_depth="advanced", max_resuslts=5, include_answer=True
+            tavily_api_key=TAVILY_API_KEY,
+            search_depth="advanced",
+            max_resuslts=5,
+            include_answer=True
         )
         self.wiki_search = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(top_k_results=5))
 
     
-    def search_topic(self, query:str, search_type:str ="all"):
-        """Search the user topic on the web or wikipedia"""
-
+    def search_topic(self, query:str, search_type:str ="all")-> List:
+        """
+            Search the user topic on the web or 
+            Args:
+                query:str =  user query
+                search_type:str = wiki or web search
+        """
         sources = []
         if search_type in ["web", "all"]:
             try:                
@@ -55,30 +61,8 @@ class SearchAgent:
 
         return sources
         
-    
-    def search(self, topic:str, urls_only=False):
-        """Search the web for sources using various queries"""
-        topics  = self._generate_diverse_topics(topic)
-        all_sources = {"topic": [], "sources": []}
-        for topic in topics:
-            all_sources["topic"].append(topic)
-            all_sources["sources"].append(
-                self.search_topic(topic)
-            )
-
-        unique_urls = set()
-        for res in all_sources["sources"]:
-            for source in res:
-                url = source["url"]
-                if url not in unique_urls:
-                    unique_urls.add(url)
-        unique_urls = [url for url in unique_urls]
-        if urls_only:
-            return unique_urls
-        else:
-            return all_sources, unique_urls
-        
-    def fast_search(self, topic:str, sort_results=True):
+          
+    def search(self, topic:str, sort_results=True):
         """Parallel search to reduce search time"""
         topics  = self._generate_diverse_topics(topic)
         all_sources = {"topic": topics, "sources": []}
@@ -131,5 +115,4 @@ class SearchAgent:
             return response.queries
         except Exception as e:
             print(f"Error: {e}")
-            return None
-    
+            return None   
